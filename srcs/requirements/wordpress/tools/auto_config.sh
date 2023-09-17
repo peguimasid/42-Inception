da8ea7464f55715e4611da7a77445655bb8a6c5c
Guilhermo Masid <guilhermomasid@gmail.com>
2023-09-17 15:43:10 -0400

    changes

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -4,8 +4,8 @@ if [ ! -e /var/www/wordpress/wp-config.php ]; then
   sleep 10
   mv /wp-config.php /var/www/wordpress
   sed -i -r "s/db1/$SQL_DATABASE/1"   /var/www/wordpress/wp-config.php
-  sed -i -r "s/user/$SQL_USER/1"  /var/www/wordpress/wp-config.php
-  sed -i -r "s/pwd/$SQL_PASSWORD/1"    /var/www/wordpress/wp-config.php
+  sed -i -r "s/user/$SQL_USER/1"      /var/www/wordpress/wp-config.php
+  sed -i -r "s/pwd/$SQL_PASSWORD/1"   /var/www/wordpress/wp-config.php
 
   wp core install --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --skip-email --allow-root --path='/var/www/wordpress'
   wp user create --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
